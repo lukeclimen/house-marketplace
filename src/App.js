145db8929c16beffb9d160458ed521203072ebc6
Luke Climenhage <luke.climenhage@gmail.com>
2022-10-03 17:26:31 -0400

    Created the webpages and added them to a router in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
 	return (
 		<>
-			<h1>My App</h1>
+			<Router>
+				<Routes>
+					<Route
+						path='/'
+						element={<Explore />}
+					/>
+					<Route
+						path='/forgotpassword'
+						element={<ForgotPassword />}
+					/>
+					<Route
+						path='/offers'
+						element={<Offers />}
+					/>
+					<Route
+						path='/profile'
+						element={<SignIn />}
+					/>
+					<Route
+						path='/sign-in'
+						element={<SignIn />}
+					/>
+					<Route
+						path='/sign-up'
+						element={<SignUp />}
+					/>
+				</Routes>
+			</Router>
+			{/* Navbar */}
 		</>
 	);
 }
